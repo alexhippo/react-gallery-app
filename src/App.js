86277c32f099583e471c7300b5e77aa3351b55d0
Alex Hipolito <alexheepo@gmail.com>
2021-08-09 15:29:35 +1000

    Install React Router, set up NavLinks in Nav component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
 import apiKey from './config.js';
+import {
+  BrowserRouter,
+  Route
+} from 'react-router-dom';
 
 // App components
 import Search from './components/Search';
@@ -8,11 +12,13 @@ import PhotoContainer from './components/PhotoContainer';
 
 function App() {
   return (
-    <div className="container">
-      <Search />
-      <Nav />
-      <PhotoContainer />
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <Search />
+        <Nav />
+        <PhotoContainer />
+      </div>
+    </BrowserRouter>
   );
 }
 
