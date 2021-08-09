f54cb2e439661fa584c063b2029384e33f4e5c91
Alex Hipolito <alexheepo@gmail.com>
2021-08-09 14:32:24 +1000

    Made PhotoContainer and Search components stateless. Fixed up NotFound component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const NotFound = props => (
+const NotFound = () => (
     <li class="not-found">
         <h3>No Results Found</h3>
         <p>You search did not return any results. Please try again.</p>
