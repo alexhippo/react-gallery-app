cb4c8e20515876e4efd372a3f409f1d6782b61eb
Alex Hipolito <alexheepo@gmail.com>
2021-08-15 17:27:28 +1000

    Update title for PhotoContainer

--- src/App.js
+++ src/App.js
@@ -61,10 +61,10 @@ class App extends Component {
         <Search onSearch={this.performSearch} />
         <Nav />
         <Switch>
-          <Route exact path="/" component={() => <PhotoContainer data={this.state.photos} />} />
-          <Route path="/munchkin-cats" component={() => <PhotoContainer data={this.state.photos} />} />
-          <Route path="/scottish-fold-cats" component={() => <PhotoContainer data={this.state.photos} />} />
-          <Route path="/british-short-hair-cats" component={() => <PhotoContainer data={this.state.photos} />} />
+          <Route exact path="/" component={() => <PhotoContainer data={this.state.photos} title={'Cats Results'} />} />
+          <Route path="/munchkin-cats" component={() => <PhotoContainer data={this.state.photos} title={'Munchkin Cats Results'} />} />
+          <Route path="/scottish-fold-cats" component={() => <PhotoContainer data={this.state.photos} title={'Scottish Fold Cats Results'} />} />
+          <Route path="/british-short-hair-cats" component={() => <PhotoContainer data={this.state.photos} title={'British Short Hair Cat Results'} />} />
           <Route exact path="/search/:query" component={() => <PhotoContainer data={this.state.photos} />} />
         </Switch>
       </div>
--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -5,7 +5,7 @@ class PhotoContainer extends Component {
     render() {
         return (
             <div className="photo-container">
-                <h2>Results</h2>
+                <h2>{this.props.title ? this.props.title : 'Results'}</h2>
                 <ul>
                     {this.props.data.map((photo) => {
                         return <Photo server={photo.server} id={photo.id} key={photo.id} secret={photo.secret} />
