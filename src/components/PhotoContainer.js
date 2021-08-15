f0244085ab1477e92ff130815ee63f6371a76fa1
Alex Hipolito <alexheepo@gmail.com>
2021-08-15 18:01:54 +1000

    Improve handle of no search results found scenario

--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -1,32 +1,23 @@
 import React, { Component } from 'react';
 import Photo from './Photo';
+import NotFound from './NotFound';
 
 class PhotoContainer extends Component {
     render() {
-        if (this.props.data.length > 0) {
-            return (
-                <div className="photo-container">
-                    <h2>{this.props.title ? this.props.title : 'Results'}</h2>
-                    <ul>
-                        {this.props.data.map((photo) => {
+        return (
+            <div className="photo-container">
+                <h2>{this.props.title ? this.props.title : 'Results'}</h2>
+                <ul>
+                    {this.props.data.length > 0 ?
+                        this.props.data.map((photo) => {
                             return <Photo server={photo.server} id={photo.id} key={photo.id} secret={photo.secret} />
-                        })}
-                    </ul>
-                </div>
-            );
-        } else {
-            return (
-                <div className="photo-container">
-                    <ul>
-                        <li class="not-found">
-                            <h3>No Results Found</h3>
-                            <p>Your search did not return any results. Please try again.</p>
-                        </li>
-                    </ul>
-                </div>
-            );
-        }
-
+                        })
+                        :
+                        <NotFound />
+                    }
+                </ul>
+            </div>
+        );
     }
 }
 
