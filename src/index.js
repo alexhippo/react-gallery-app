b173cfa7fea5981b50e2824407463528d7cd48a4
Alex Hipolito <alexheepo@gmail.com>
2022-03-07 23:20:50 +1100

    Update dependencies, deploy to Github Pages

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/react-gallery-app">
     <App />
   </BrowserRouter>,
   document.getElementById('root')
